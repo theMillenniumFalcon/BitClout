16db37572d65818f58c00c6cf3946c3814045174
Nishank <nishankpr2002@gmail.com>
2021-12-22 11:32:37 +0530

    mikroORM_added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants"
+import { Post } from "./entities/Post"
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'myreddit',
+        type: 'postgresql',
+        debug: !__prod__,
+    })
+
+    const post = orm.em.create(Post, {title: 'my first post'})
+    await orm.em.persistAndFlush(post)
+    console.log('--------------------')
+    await orm.em.nativeInsert(Post, {title: 'my first post 2'})
+}
+main().catch((error) => {
+    console.error(error)
+})
