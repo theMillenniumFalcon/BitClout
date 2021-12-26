137279634a1abbf8e761d9a51e9704d2839bb948
Nishank <nishankpr2002@gmail.com>
2021-12-26 16:33:36 +0530

    urql added

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 import InputField from '../components/InputField'
 import Wrapper from '../components/Wrapper'
 import { Box, Button } from "@chakra-ui/react"
+import { useMutation } from 'urql'
 
-interface registerProps {
-
-}
+interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+    const [] = useMutation(``)
     return (
         <Wrapper variant="small">
             <Formik initialValues={{ username: '', password: '' }} onSubmit={(values) => {
