98ee6d51f2c90fe59a15844ba243cbd31fe06c4a
Nishank <nishankpr2002@gmail.com>
2022-01-15 01:23:01 +0530

    graphql added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,45 @@
 require('dotenv').config()
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants"
-import { Post } from "./entities/Post"
+// import { Post } from "./entities/Post"
 import microConfig from "./mikro-orm.config"
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello"
+import { PostResolver } from "./resolvers/Post"
+
+const PORT = process.env.PORT || 4000
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig)
+    await orm.getMigrator().up()
 
-    const post = orm.em.create(Post, {title: 'my first post'})
-    await orm.em.persistAndFlush(post)
-    console.log('-------sql 2-------')
-    await orm.em.nativeInsert(Post, {title: 'my first post 2'})
+    const app = express()
+
+    app.get('/', (_, res) => {
+        res.send("Server is working fine!")
+    })
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({ app })
+
+    const server = app.listen(PORT, () => {
+        console.log(`listening on port ${PORT}`)
+    })
+
+    process.on('unhandledRejection', (err, _) => {
+        console.log(`Logged Error: ${err}`)
+        server.close(() => process.exit(1))
+    })
+    
 }
 main().catch((error) => {
     console.error(error)
