e17e3433c9e99666970b6ce07b8e1de46bd2f484
Nishank <nishankpr2002@gmail.com>
2022-01-16 14:55:13 +0530

    crud added

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -5,19 +5,57 @@ import { MyContext } from "../types";
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
-    posts(@Ctx() ctx: MyContext): Promise<Post[]> {
+    posts(
+        @Ctx() ctx: MyContext
+    ): Promise<Post[]> {
         return ctx.em.find(Post, {})
     }
 
     @Query(() => Post, { nullable: true })
-    post(@Arg('id') id: number, @Ctx() ctx: MyContext): Promise<Post | null> {
+    post(
+        @Arg('id') id: number, 
+        @Ctx() ctx: MyContext
+    ): Promise<Post | null> {
         return ctx.em.findOne(Post, { id })
     }
 
     @Mutation(() => Post)
-    async createPost(@Arg('title') title: string, @Ctx() ctx: MyContext): Promise<Post> {
+    async createPost(
+        @Arg('title') title: string, 
+        @Ctx() ctx: MyContext
+    ): Promise<Post> {
         const post = ctx.em.create(Post, {title})
         await ctx.em.persistAndFlush(post)
         return post
+    }
+
+    @Mutation(() => Post, { nullable: true })
+    async updatePost(
+        @Arg('id') id: number, 
+        @Arg('title', () => String, { nullable: true }) title: string, 
+        @Ctx() ctx: MyContext
+    ): Promise<Post | null> {
+        const post = await ctx.em.findOne(Post, {id})
+        if (!post) {
+            return null
+        }
+        if (typeof title !== 'undefined') {
+            post.title = title
+            await ctx.em.persistAndFlush(post)
+        }
+        return post
+    }
+
+    @Mutation(() => Boolean)
+    async deletePost(
+        @Arg('id') id: number, 
+        @Ctx() ctx: MyContext
+    ): Promise<boolean> {
+        try {
+            await ctx.em.nativeDelete(Post, { id })
+        } catch {
+            return false
+        }
+        return true
     }
 }
