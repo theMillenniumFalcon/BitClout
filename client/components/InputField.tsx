9e02b9fc272565d6c11c158c2545cb21acb3587b
Nishank <nishankpr2002@gmail.com>
2022-01-17 23:30:01 +0530

    register added

---
+++ client/components/InputField.tsx
@@ -0,0 +1,24 @@
+import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react'
+import { useField } from 'formik'
+import React, { InputHTMLAttributes } from 'react'
+
+type inputfieldprops = InputHTMLAttributes<HTMLInputElement> & {
+    label: string
+    name: string
+}
+
+// * '' => false
+// * 'error message stuff' => true
+
+const InputField: React.FC<inputfieldprops> = ({label, size, ...props}) => {
+    const [field, {error}] = useField(props)
+    return (
+        <FormControl isInvalid={!!error}>
+            <FormLabel htmlFor={field.name}>{label}</FormLabel>
+            <Input {...field} {...props} id={field.name} />
+            {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+        </FormControl>
+    )
+}
+
+export default InputField
