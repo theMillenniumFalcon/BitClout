369aa3a53da190b09c7d852d40d04bb17dda8bd4
Nishank <nishankpr2002@gmail.com>
2022-01-23 14:56:00 +0530

    auth completed

--- client/pages/forgot-password.tsx
+++ client/pages/forgot-password.tsx
@@ -1,9 +1,33 @@
-import React from "react"
+import { Box, Button } from "@chakra-ui/react"
+import { Formik, Form } from "formik"
+import { withUrqlClient } from "next-urql"
+import React, { useState } from "react"
+import InputField from "../components/InputField"
+import Wrapper from "../components/Wrapper"
+import { useForgotPasswordMutation } from "../generated/graphql"
+import { createUrqlClient } from "../utils/createUrqlClient"
 
 const ForgotPassword: React.FC<{}> = ({}) => {
+    const [complete, setComplete] = useState(false)
+    const [, forgotPassword] = useForgotPasswordMutation()
     return (
-        <div>Hi</div>
+        <Wrapper variant="small">
+            <Formik initialValues={{ email: "" }}
+                onSubmit={async (values) => {
+                    await forgotPassword(values)
+                    setComplete(true)
+                }}>
+                {({ isSubmitting }) => complete ? (
+                    <Box>Check your inbox</Box>
+                ) : (
+                    <Form>
+                        <InputField name="email" placeholder="email" label="Email" />
+                        <Button mt={4} type='submit' colorScheme='teal' isLoading={isSubmitting}>Forgot Password</Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
     )
 }
 
-export default ForgotPassword
+export default withUrqlClient(createUrqlClient)(ForgotPassword)
