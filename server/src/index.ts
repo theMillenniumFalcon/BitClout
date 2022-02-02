8974fa7a0324d5d43da24577fc9530291454b760
Nishank <nishankpr2002@gmail.com>
2022-02-02 18:49:04 +0530

    fixed something

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,6 +33,8 @@ const main = async () => {
     })
     await connection.runMigrations()
 
+    // await Post.delete({})
+
     const app = express()
 
     const RedisStore = connnectRedis(session)
